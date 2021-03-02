a33a0c939d6a07dfb0b5608d28ce7ceda0eeaa8c
cjg <43872475+cojguerrero@users.noreply.github.com>
2021-03-02 01:06:59 -0800

    Added CLI arguments

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pandas as pd
+import argparse
 import os
+import sys
 
 import googleapiclient.discovery
 
 
-def get_videos(token=None):
+def get_videos(playlistId, token=None):
 
     request = youtube.playlistItems().list(
         part="snippet",
-        playlistId="PLBCF2DAC6FFB574DE",
+        playlistId=playlistId,
         pageToken=token
     )
     response = request.execute()
@@ -17,7 +19,7 @@ def get_videos(token=None):
         snippet = video["snippet"]
 
         data["Title"].append(snippet["title"])
-        data["Video ID"].append(snippet["resourceId"]["videoId"])
+        data["Video ID"].append("https://www.youtube.com/watch?v=" + snippet["resourceId"]["videoId"])
         data["Published At"].append(snippet["publishedAt"])
 
         if snippet["thumbnails"]:
@@ -26,30 +28,53 @@ def get_videos(token=None):
             data["Thumbnail"].append(None)
 
     if "nextPageToken" in response:
-        return get_videos(token=response["nextPageToken"])
+        return get_videos(playlistId=playlistId, token=response["nextPageToken"])
 
 
-# Disable OAuthlib's HTTPS verification when running locally.
-# *DO NOT* leave this option enabled in production.
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+if __name__ == '__main__':
 
-api_service_name = "youtube"
-api_version = "v3"
-DEVELOPER_KEY = "AIzaSyCFWXRyybHEGSra3vmTJxonKBa0yVyk9rg"
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-youtube = googleapiclient.discovery.build(
-    api_service_name, api_version, developerKey = DEVELOPER_KEY)
+    # set up YouTube API call
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = os.environ.get("YT_DEV_KEY")
 
-data = {
-    "Title": [],
-    "Video ID": [],
-    "Published At": [],
-    "Thumbnail": []
-}
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey = DEVELOPER_KEY)
 
-# get data on each video in playlist
-get_videos()
+    # create empty dict to store data
+    data = {
+        "Title": [],
+        "Video ID": [],
+        "Published At": [],
+        "Thumbnail": []
+    }
 
-df = pd.DataFrame(data)
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--playlist_url',
+        type=str,
+        default='',
+        help='Full YouTube URL of playlist.'
+    )
+    parser.add_argument(
+        '--save_path',
+        type=str,
+        default='playlist_videos.csv',
+        help='File to save the playlist data to.'
+    )
 
-print(df)
+    FLAGS, unparsed = parser.parse_known_args()
+
+    # get data on each video in playlist
+    url = FLAGS.playlist_url.split('playlist?list=')[-1]
+    get_videos(playlistId=url)
+
+    df = pd.DataFrame(data)
+
+    # save data to CSV file at location specified 
+    df.to_csv(FLAGS.save_path)
