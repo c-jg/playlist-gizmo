dd81fa08f1d8177e138f7ab68d42573c9c19459e
cjg <43872475+cojguerrero@users.noreply.github.com>
2021-03-03 21:33:40 -0800

    Move local file handling to external script

---
+++ watch_later_html.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup as soup
+
+
+def use_local_file(FLAGS):
+    '''
+    Reads all Watch Later data in from HTML file.
+    Saves video titles and urls.
+    '''
+
+    data = {
+        "Title": [],
+        "URL": []
+    }
+
+    file_name = FLAGS.local_file
+    html = soup(open(file_name), "html.parser")
+    videos = html.find_all("a", {"id":"video-title"})
+
+    for video in videos:
+        data["Title"].append(video["title"])
+        data["URL"].append("https://www.youtube.com" + video["href"].split("&list=WL")[0])
+
+    return data
